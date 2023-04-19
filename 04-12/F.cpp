cf85906ac48321c733e1b9aa4ba574ee7dbd9fc7
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2023-04-20 01:51:13 +0900

    3333

--- 04-12/F.cpp
+++ 04-12/F.cpp
@@ -11,6 +11,30 @@
 
 // 4999
 
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main()
+// {
+//     string able;
+//     string want;
+
+//     getline(cin, able, '\n');
+//     getline(cin, want, '\n');
+
+//     if (able.size() >= want.size())
+//         cout << "go";
+//     else
+//         cout << "no";
+// }
+
+// 1264
+
+// 풀기 위해 필요한 것
+// While, if, foreach, ArrayPush
+
 #include <iostream>
 #include <string>
 
@@ -18,14 +42,40 @@ using namespace std;
 
 int main()
 {
-    string able;
-    string want;
+    while (true)
+    {
+        string STR;
+        int cnt = 0;
 
-    getline(cin, able, '\n');
-    getline(cin, want, '\n');
+        getline(cin, STR, '\n');
 
-    if (able.size() >= want.size())
-        cout << "go";
-    else
-        cout << "no";
+        if (STR == "#")
+            break;
+
+        for (int i = 0; i < STR.length(); i++)
+        {
+            if (STR[i] == 'a')
+                cnt++;
+            if (STR[i] == 'e')
+                cnt++;
+            if (STR[i] == 'i')
+                cnt++;
+            if (STR[i] == 'o')
+                cnt++;
+            if (STR[i] == 'u')
+                cnt++;
+            if (STR[i] == 'A')
+                cnt++;
+            if (STR[i] == 'E')
+                cnt++;
+            if (STR[i] == 'I')
+                cnt++;
+            if (STR[i] == 'O')
+                cnt++;
+            if (STR[i] == 'U')
+                cnt++;
+        }
+        cout << cnt << endl;
+        cnt = 0;
+    }
 }
