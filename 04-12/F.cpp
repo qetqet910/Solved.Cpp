3904a92fd0b31fdfe34f4c5cfc967c9a92b44f2f
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2023-04-19 17:40:34 +0900

    4999

--- 04-12/F.cpp
+++ 04-12/F.cpp
@@ -1,9 +1,31 @@
 // 2420
-#include <iostream>
-#include <math.h>
 
-int main(){
-    long a, b;
-    std::cin >> a >> b;
-    std::cout << abs(a - b);
+// #include <iostream>
+// #include <math.h>
+
+// int main(){
+//     long a, b;
+//     std::cin >> a >> b;
+//     std::cout << abs(a - b);
+// }
+
+// 4999
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string able;
+    string want;
+
+    getline(cin, able, '\n');
+    getline(cin, want, '\n');
+
+    if (able.size() >= want.size())
+        cout << "go";
+    else
+        cout << "no";
 }
