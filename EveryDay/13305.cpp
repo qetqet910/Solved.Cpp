d43c2b5cf664119eeba8e4e1b8efd6465ad6ce89
5:14 <79036088+qetqet910@users.noreply.github.com>
2024-06-29 15:29:07 +0900

    Create 13305.cpp

---
+++ EveryDay/13305.cpp
@@ -0,0 +1,33 @@
+// 각 단계에서 최적이라고 생각되는 것을 선택, 최적의 값의 근사한 값이 목표
+// 탐욕 선택 속성 - 각 단계에서 최적의 선택을 했을 때 최적해가 나와야함
+// 최적 부분 구조 - 전체의 최적해가 부분 문제의 최적해로 구성
+
+#include <iostream>
+
+using namespace std;
+
+long road[100001];
+long cost[100001];
+
+int main(){
+    int n;
+    long long cNow = 0, result = 0;
+    cin >> n;
+
+    for(int i = 0; i < n-1; i++){long a; cin >> a; road[i] = a;}
+    for(int i = 0; i < n; i++){long a; cin >> a; cost[i] = a;}
+
+    cNow = cost[0];
+    result = cost[0] * road[0];
+
+    for(int i = 1; i < n; i++){
+        if(cNow < cost[i]){
+            result += cNow * road[i];
+        }else{
+            cNow = cost[i];
+            result += cNow * road[i];
+        }
+    }
+    
+    cout << result;
+}
